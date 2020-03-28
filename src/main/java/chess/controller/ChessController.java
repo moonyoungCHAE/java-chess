c612c0463a60e800c0bd33944ee2f11d26d9908e
Seyun <ksy90101@gmail.com>
2020-03-28 14:44:59 +0900

    refactor : 입력 로직에 대한 리팩토링

--- src/main/java/chess/controller/ChessController.java
+++ src/main/java/chess/controller/ChessController.java
@@ -5,34 +5,35 @@ import chess.domain.position.Positions;
 import chess.views.InputView;
 import chess.views.OutputView;
 
+import java.util.Map;
+
 public class ChessController {
     public void play() {
         OutputView.printInitialGuide();
+        Command command = Command.of(InputView.inputCommand().get("commend"));
 
-        if (!Command.of(InputView.inputCommand()).equals(Command.START)) {
+        if (command != Command.START) {
             throw new IllegalArgumentException("start를 해야 합니다.");
         }
 
-        // 정상적으로 들어왔을 때
         ChessBoard chessBoard = new ChessBoard();
         OutputView.printChessBoard(chessBoard.getChessBoard());
-        while (true) {
-            String input = InputView.inputCommand();
+
+        do {
+            Map<String, String> inputs = InputView.inputCommand();
             // (to do) 입력 ~ 배열에 대한 처리
             // (to do) move a2 a4 3칸만 입력했는지 검증
-            Command command = Command.of(input.split(" ")[0]);
+            command = Command.of(inputs.get("commend"));
 
             // (to do) state 상태 관리 필요하다.
             if (command.equals(Command.MOVE)) {
-                chessBoard.move(Positions.of(input.split(" ")[1]), Positions.of(input.split(" ")[2]));
+                chessBoard.move(Positions.of(inputs.get("from")), Positions.of(inputs.get("to")));
                 OutputView.printChessBoard(chessBoard.getChessBoard());
             }
+
             if (command.equals(Command.STATUS)) {
                 OutputView.printStatus(chessBoard.getResult());
             }
-            if (command.equals(Command.END)) {
-                return;
-            }
-        }
+        } while (!command.equals(Command.END));
     }
 }
