b22c5987f529c869e9268e44d75a4507286c829a
Seyun <ksy90101@gmail.com>
2020-03-28 16:22:23 +0900

    refactor : controller while문을 do while로 변경

--- src/main/java/chess/views/InputView.java
+++ src/main/java/chess/views/InputView.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
     private static final String DELIMITER = " ";
-    private static final String COMMEND = "commend";
+    private static final String COMMAND = "command";
     private static final String FROM = "from";
     private static final String TO = "to";
 
@@ -15,12 +15,12 @@ public class InputView {
         String string = scanner.nextLine();
         Map<String, String> stringMap = new HashMap<>();
         if (string.split(DELIMITER).length == 1) {
-            stringMap.put(COMMEND, string);
+            stringMap.put(COMMAND, string);
             return stringMap;
         }
-        stringMap.put(COMMEND, string.split(" ")[0]);
-        stringMap.put(FROM, string.split(" ")[1]);
-        stringMap.put(TO, string.split(" ")[2]);
+        stringMap.put(COMMAND, string.split(DELIMITER)[0]);
+        stringMap.put(FROM, string.split(DELIMITER)[1]);
+        stringMap.put(TO, string.split(DELIMITER)[2]);
         return stringMap;
     }
 }
